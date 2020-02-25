ad7a5633396148f9841f375fe5adbf9a59693a03
Kengo Nakajima <kengo.nakajima@gmail.com>
2020-02-25 23:37:29 +0900

    g

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
-g++ -c util.cpp 
-g++ -o cl cl.cpp libstunmsg.a util.o
-g++ -o sig sig.cpp util.o
+rm cl sig *.o
+g++ -g -c util.cpp 
+g++ -g -o cl cl.cpp libstunmsg.a util.o
+g++ -g -o sig sig.cpp util.o
